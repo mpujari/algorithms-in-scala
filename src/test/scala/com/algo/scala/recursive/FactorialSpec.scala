772fac11bb2b430297b1c9183ae6519b9356ea63
mpujari <pujarimahesh_kumar@yahoo.com>
2019-04-29 10:55:03 +0530

    Simplifing code in FactorialSpec

--- src/test/scala/com/algo/scala/recursive/FactorialSpec.scala
+++ src/test/scala/com/algo/scala/recursive/FactorialSpec.scala
@@ -1,44 +1,41 @@
 package com.algo.scala.recursive
 
 import org.scalatest.FlatSpec
+import com.algo.scala.recursive.Factorial._
 
 class FactorialSpec extends FlatSpec {
 
+  val values = Map(
+    1 -> 1,
+    2 -> 2,
+    3 -> 6,
+    5 -> 120,
+    10 -> (2 to 10).product,
+    30 -> (2 to 30).product
+  )
+
   "factorial test with fact0" should "pass" in {
-    assert(Factorial.fact0(1) == 1)
-    assert(Factorial.fact0(2) == 2)
-    assert(Factorial.fact0(3) == 6)
-    assert(Factorial.fact0(5) == 120)
-    assert(Factorial.fact0(10) == (2 to 10).product)
-    assert(Factorial.fact0(30) == (2 to 30).product)
+    values foreach { e =>
+      assert(fact0(e._1) == e._2)
+    }
   }
 
   "factorial test with fact1" should "pass" in {
-    assert(Factorial.fact1(1) == 1)
-    assert(Factorial.fact1(2) == 2)
-    assert(Factorial.fact1(3) == 6)
-    assert(Factorial.fact1(5) == 120)
-    assert(Factorial.fact1(10) == (2 to 10).product)
-    assert(Factorial.fact1(30) == (2 to 30).product)
+    values foreach { e =>
+      assert(fact1(e._1) == e._2)
+    }
   }
 
   "factorial test with fact2" should "pass" in {
-    assert(Factorial.fact2(1) == 1)
-    assert(Factorial.fact2(2) == 2)
-    assert(Factorial.fact2(3) == 6)
-    assert(Factorial.fact2(5) == 120)
-    assert(Factorial.fact2(10) == (2 to 10).product)
-    assert(Factorial.fact2(30) == (2 to 30).product)
+    values foreach { e =>
+      assert(fact2(e._1) == e._2)
+    }
   }
 
   "factorial test with fact3" should "pass" in {
-    assert(Factorial.fact3(1) == 1)
-    assert(Factorial.fact3(2) == 2)
-    assert(Factorial.fact3(3) == 6)
-    assert(Factorial.fact3(5) == 120)
-    assert(Factorial.fact3(10) == (2 to 10).product)
-    assert(Factorial.fact3(30) == (2 to 30).product)
+    values foreach { e =>
+      assert(fact3(e._1) == e._2)
+    }
   }
-
 
 }
